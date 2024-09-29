f4f3e8a43ec5a49e51988229a56f8adb7683da96
astimergalieva <astimergalieva@domclick.ru>
2024-09-29 15:26:16 +0300

    add logger middleware test

--- internal/logging/middleware_test.go
+++ internal/logging/middleware_test.go
@@ -1,98 +1,90 @@
-// Package logging реализует middleware, логгирующую информацию о сервисе
 package logging
 
 import (
+	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
 
-	"go.uber.org/zap"
+	"gotest.tools/assert"
 )
 
-func Test_loggingResponseWriter_WriteHeader(t *testing.T) {
-	type fields struct {
-		ResponseWriter http.ResponseWriter
-		responseData   *responseData
-	}
-	type args struct {
-		statusCode int
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		args   args
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			r := &loggingResponseWriter{
-				ResponseWriter: tt.fields.ResponseWriter,
-				responseData:   tt.fields.responseData,
-			}
-			r.WriteHeader(tt.args.statusCode)
-		})
-	}
+type TestMiddleWare struct {
+	t *testing.T
 }
 
-func Test_loggingResponseWriter_Write(t *testing.T) {
-	type fields struct {
-		ResponseWriter http.ResponseWriter
-		responseData   *responseData
+func (m TestMiddleWare) Handle(next http.Handler) http.Handler {
+
+	test := func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r)
+
+		v := reflect.ValueOf(w)
+
+		ind := reflect.Indirect(v)
+
+		d := ind.FieldByName("responseData")
+		data := reflect.Indirect(d)
+
+		fmt.Println(data)
+		size := data.FieldByName("size")
+
+		var expectedSize int64 = 8
+		assert.Equal(m.t, expectedSize, size.Int())
+
+		code := data.FieldByName("status")
+		var expectedStatus int64 = 200
+		assert.Equal(m.t, expectedStatus, code.Int())
+
+		resp := data.FieldByName("data")
+		expectedData := "response"
+		assert.Equal(m.t, expectedData, string(resp.Bytes()))
 	}
-	type args struct {
-		b []byte
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    int
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			r := &loggingResponseWriter{
-				ResponseWriter: tt.fields.ResponseWriter,
-				responseData:   tt.fields.responseData,
-			}
-			got, err := r.Write(tt.args.b)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("loggingResponseWriter.Write() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("loggingResponseWriter.Write() = %v, want %v", got, tt.want)
-			}
-		})
-	}
+
+	result := http.HandlerFunc(test)
+
+	lo, _ := NewLogger()
+	return lo.Handle(result)
+}
+
+type Mockhandler struct {
+	t *testing.T
+}
+
+func (m *Mockhandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	_, _ = w.Write([]byte("response"))
 }
 
 func TestLogger_Handle(t *testing.T) {
-	type fields struct {
-		sugar *zap.SugaredLogger
-	}
+
 	type args struct {
-		h http.Handler
+		next http.Handler
 	}
+
+	r := httptest.NewRequest(http.MethodGet, "/api/", nil)
+
+	handler := &Mockhandler{t}
+
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
-		want   http.Handler
+		name    string
+		args    args
+		request *http.Request
 	}{
-		// TODO: Add test cases.
+		{"good request", args{handler}, r},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			l := Logger{
-				sugar: tt.fields.sugar,
-			}
-			if got := l.Handle(tt.args.h); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Logger.Handle() = %v, want %v", got, tt.want)
-			}
+
+			tm := TestMiddleWare{t}
+			h := tm.Handle(tt.args.next)
+
+			w := httptest.NewRecorder()
+			h.ServeHTTP(w, tt.request)
+
+			assert.Equal(t, 200, w.Code)
+			assert.Equal(t, "response", w.Body.String())
 		})
 	}
 }
